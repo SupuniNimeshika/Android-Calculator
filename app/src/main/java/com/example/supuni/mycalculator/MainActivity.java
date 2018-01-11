d82d090e201e40f2040be98ec6dea9b855e49672
Your Name <supuni.nimeshika@gmail.com>
2018-01-11 20:52:29 +0530

    Write Function on Number

--- app/src/main/java/com/example/supuni/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/supuni/mycalculator/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.supuni.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView screen;
+    private String display="";
+    private String currentOperator="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screen = (TextView)findViewById(R.id.editText);
+        screen.setText(display);
+    }
+
+    private void updateScreen(){
+        screen.setText(display);
+    }
+
+    public void onClickNumber (View v){
+        Button b= (Button) v;
+        display += b.getText();
+        updateScreen();
+    }
+
+    public void onClickOperator(View v){
+        Button b =(Button)v;
+        display +=b.getText();
+        currentOperator=b.getText().toString();
+        updateScreen();
+    }
+
+    public void clear(){
+        display ="";
+        currentOperator="";
+        updateScreen();
+    }
+
+    public void onClickClear(View v){
+        clear();
+        updateScreen();
+    }
+
+    public void onClickEqual(View v){
+
     }
 }
